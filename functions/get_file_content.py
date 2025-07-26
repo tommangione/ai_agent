9291c75f8b838775f0203d631e569bed58777cd8
tommangione <tommangione@pm.me>
2025-07-26 14:55:52 -0500

    Added get_file_content function and config file for functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,3 +1,21 @@
+import os
+from functions.config import *
+
 def get_file_content(working_directory, file_path):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_working_directory = os.path.abspath(working_directory)
 
+    if abs_file_path.startswith(abs_working_directory) == False:
+        return(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
 
+    if os.path.isfile(abs_file_path) == False:
+        return(f'Error: File not found or is not a regular file: "{file_path}"')
+
+    try:
+        with open(abs_file_path) as f: content = f.read()
+        if len(content) > CHAR_LIMIT:
+            content = content[0:CHAR_LIMIT]
+            content = content + f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+    except Exception as e:
+        return(f"Error: {e}")
--- tests.py
+++ tests.py
@@ -1,13 +1,24 @@
-from functions.get_files_info import get_files_info
+#from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
+#print("Result for current directory:")
+#print(get_files_info("calculator", "."))
 
-print("Result for 'pkg' directory:")
-print(get_files_info("calculator", "pkg"))
+#print("Result for 'pkg' directory:")
+#print(get_files_info("calculator", "pkg"))
 
-print("Result for '/bin' directory:")
-print(get_files_info("calculator", "/bin"))
+#print("Result for '/bin' directory:")
+#print(get_files_info("calculator", "/bin"))
 
-print("Result for '../' directory:")
-print(get_files_info("calculator", "../"))
+#print("Result for '../' directory:")
+#print(get_files_info("calculator", "../"))
+
+#print(get_file_content("calculator", "lorem.txt"))
+
+print(get_file_content("calculator", "main.py"))
+
+print(get_file_content("calculator", "pkg/calculator.py"))
+
+print(get_file_content("calculator", "/bin/cat"))
+
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
