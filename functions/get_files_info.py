988a30a57c08bd25798c258c3335aab315834dfd
tommangione <tommangione@pm.me>
2025-07-26 14:05:11 -0500

    Added function get_files_info

---
+++ functions/get_files_info.py
@@ -0,0 +1,37 @@
+# from DIRNAME.FILENAME import FUNCTION_NAME
+import os
+
+def get_files_info(working_directory, directory="."):
+    # directory parameter is treated as a relative path within the working directory
+    dir_full_path = os.path.join(working_directory, directory)
+    dir_absolute_path = os.path.abspath(dir_full_path)
+    wkdir_absolute_path = os.path.abspath(working_directory)
+    # Error handling:
+
+    if dir_absolute_path.startswith(wkdir_absolute_path) == False:
+        return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    
+    if os.path.isdir(dir_absolute_path) == False:
+        return(f'Error: "{directory}" is not a directory')
+
+    # The goal: to build and return a string representing the contents of the directory
+    # example for what's returned:
+    # - README.md: file_size=1032 bytes, is_dir=False
+    # - src: file_size=128 bytes, is_dir=True
+    # - package.json: file_size=1234 bytes, is_dir=False
+    try:
+        dir_contents = os.listdir(dir_absolute_path)
+        output_block = ""
+        if len(dir_contents) == 0:
+            return("")
+        for object in dir_contents:
+            object_path = os.path.join(dir_absolute_path, object)
+            object_size = os.path.getsize(object_path)
+            if os.path.isdir(object_path) == True:
+                is_dir = True
+            else:
+                is_dir = False
+            output_block = output_block + f"- {object}: file_size={object_size} bytes, is_dir={is_dir}\n"  
+        return output_block
+    except Exception as e:
+        return(f"Error: {e}")
